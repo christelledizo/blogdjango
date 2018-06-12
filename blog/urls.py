86ebbe3c9b7109d8d992d00221ed27a44af4c040
ANDREE <ANDREE@DIZOANDREE.DIZOANDREE>
2018-06-12 15:05:45 +0100

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+	path('post/', views.post_list, name='post_list'), 
+	path('<int:pk>/', views.post_detail, name='post_detail'),
+	path('post/new/',views.post_new, name='post_new' ),
+	path('<int:pk>/', views.post_edit, name='post_edit'),
+	]
